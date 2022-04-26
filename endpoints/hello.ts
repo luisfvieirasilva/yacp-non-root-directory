6b2d87d55faf604e878f4af9216cf77cf3870af1
Luis Silva <luisfvieirasilva@gmail.com>
2022-04-26 14:42:29 -0300

    fix: cant change db on gets

--- endpoints/hello.ts
+++ endpoints/hello.ts
@@ -4,12 +4,17 @@ import { log, LogLevel } from "../utils/log";
 type Handler = (req: Request, res: Response) => Response | Promise<Response>
 
 const handleGet: Handler = async req => {
+  return responseFromJson({hello: "Hello 2022-04-26 14:41"})
+}
+
+const handlePost: Handler = async req => {
   await log(LogLevel.INFO, `Sending hello to ${req.headers["referer"]}`);
-  return responseFromJson({hello: "Hello 2022-04-26 12:04"})
+  return responseFromJson({hello: "Hello 2022-04-26 14:41"})
 }
 
 const handlers: Record<string, Handler> = {
   GET: handleGet,
+  POST: handlePost,
 }
 
 export default async function chisel(req: Request, res: Response) {
