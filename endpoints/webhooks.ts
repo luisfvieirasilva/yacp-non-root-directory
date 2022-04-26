67ab19d7508542b555932d1592b7c17399703b00
Luis Silva <luisfvieirasilva@gmail.com>
2022-04-26 15:10:07 -0300

    add more log on webhooks

--- endpoints/webhooks.ts
+++ endpoints/webhooks.ts
@@ -17,27 +17,43 @@ const isEventBranchValid = async (
   ref: string,
   projectBranches: string
 ): Promise<boolean> => {
+  await log(
+    LogLevel.INFO,
+    `Checking if is event branch valid. ${JSON.stringify({
+      eventType,
+      ref,
+      projectBranches,
+    })}`
+  );
   if (eventsToCheckBranches.has(eventType)) {
-    await log(LogLevel.INFO, `Checking if the ${eventType} event has a valid target branch`);
+    await log(
+      LogLevel.INFO,
+      `Checking if the ${eventType} event has a valid target branch`
+    );
     if (eventType === "push") {
       const splitRef = (ref as string).split("/");
       const refType = splitRef[1];
       if (refType == "heads") {
         const branch = splitRef.slice(2).join("/");
         if (!projectBranches.includes(branch)) {
-          await log(LogLevel.INFO,
+          await log(
+            LogLevel.INFO,
             `Branch ${branch} not included in target branches: ${projectBranches}`
           );
           return false;
         }
       } else {
-        await log(LogLevel.INFO,
+        await log(
+          LogLevel.INFO,
           `Not a 'heads' git ref, don't know what to do! Ignoring event...`
         );
         return false;
       }
     } else {
-      await log(LogLevel.INFO, `Unsupported event ${eventType}. Ignoring event...`);
+      await log(
+        LogLevel.INFO,
+        `Unsupported event ${eventType}. Ignoring event...`
+      );
       return false;
     }
   }
@@ -48,7 +64,8 @@ const postHandler: Handler = async (req) => {
   const { signature256, eventType, hookId, delivery } = parseGithubHeaders(
     req.headers
   );
-  await log(LogLevel.INFO,
+  await log(
+    LogLevel.INFO,
     `Received event with delivery id ${delivery} and type ${eventType} from Github`
   );
   const text = await req.text();
@@ -56,27 +73,20 @@ const postHandler: Handler = async (req) => {
   const { ref, repository, organization, sender } = JSON.parse(text);
   let account = "";
   if (organization == null) {
-    await log(LogLevel.INFO, `Event is not from an organization, using sender info`);
+    await log(
+      LogLevel.INFO,
+      `Event is not from an organization, using sender info`
+    );
     account = sender.login;
   } else {
     await log(LogLevel.INFO, `Event is from an organization, using org info`);
     account = organization.login;
   }
-  const projectInfo = {
-    githubRepository: repository.name,
-    organization: account,
-  };
-  await log(LogLevel.INFO,
-    `Checking if the there is a project under:\n${JSON.stringify(
-      projectInfo,
-      undefined,
-      2
-    )}`
-  );
 
   let projectBranches: string = ["main"].toString();
 
   if (!(await isEventBranchValid(eventType, ref, projectBranches))) {
+    await log(LogLevel.INFO, `Event ignored!`);
     return new Response(`Event ignored!`, { status: 200 });
   }
 
